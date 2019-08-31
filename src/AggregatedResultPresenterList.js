1ec270674ffa31038d23facde4776df2ea9ed2df
Balazs Mate Petro <petrobalazsmate@gmail.com>
2019-08-31 14:26:23 +0200

    setting up prop types

--- src/AggregatedResultPresenterList.js
+++ src/AggregatedResultPresenterList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AggregatedResultPresenter from './AggregatedResultPresenter';
 
 function AggregatedResultPresenterList(props) {
@@ -9,5 +10,10 @@ function AggregatedResultPresenterList(props) {
         </div>
     );
 }
+
+AggregatedResultPresenterList.propTypes = {
+    numberOfClicks: PropTypes.number.isRequired,
+    numberOfImpressions: PropTypes.number.isRequired
+};
 
 export default AggregatedResultPresenterList;
