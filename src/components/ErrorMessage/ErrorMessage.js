65d7c070c53d21ef4286c30e6212344e8deaaf45
Balazs Mate Petro <petrobalazsmate@gmail.com>
2019-09-01 11:30:32 +0200

    some changes on the 'ErrorMessage' component

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './ErrorMessage.scss';
 
 /**
  * @param {{message: String}} props The props of the component.
  */
 function ErrorMessage(props) {
     return (
-        <div className="error-message">{props.message}</div>
+        <div className="error-message">
+            <div className="error-message-header">Oh, something went wrong...</div>
+            <div className="error-message-body">{props.message}</div>
+        </div>
     );
 }
 
